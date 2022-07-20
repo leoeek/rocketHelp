4bb27ea51c907f8d4c4fea792efeb7fadbb2ded9
Leo <leonardolp@gmail.com>
2022-07-20 12:10:13 -0300

    Start

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { NativeBaseProvider } from 'native-base';
+import React from 'react';
+
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <SignIn />
+    </NativeBaseProvider>
+  );
+}
